7f80a16af2e09ea0e305f03f84b5c7df96267edb
Farhan Basheer <farhanfb6@outlook.com>
2021-02-08 09:48:39 +0500

    Commit All.

---
+++ Autodownloader/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace Autodownloader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnStart = new System.Windows.Forms.Button();
+            this.BtnStop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BtnStart
+            // 
+            this.BtnStart.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.BtnStart.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.BtnStart.Font = new System.Drawing.Font("Montserrat SemiBold", 8.249999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnStart.ForeColor = System.Drawing.Color.White;
+            this.BtnStart.Location = new System.Drawing.Point(254, 165);
+            this.BtnStart.Name = "BtnStart";
+            this.BtnStart.Size = new System.Drawing.Size(286, 45);
+            this.BtnStart.TabIndex = 0;
+            this.BtnStart.Text = "Start";
+            this.BtnStart.UseVisualStyleBackColor = false;
+            this.BtnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // BtnStop
+            // 
+            this.BtnStop.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.BtnStop.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.BtnStop.Font = new System.Drawing.Font("Montserrat SemiBold", 8.249999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnStop.ForeColor = System.Drawing.Color.White;
+            this.BtnStop.Location = new System.Drawing.Point(254, 216);
+            this.BtnStop.Name = "BtnStop";
+            this.BtnStop.Size = new System.Drawing.Size(286, 45);
+            this.BtnStop.TabIndex = 0;
+            this.BtnStop.Text = "Stop";
+            this.BtnStop.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BtnStop);
+            this.Controls.Add(this.BtnStart);
+            this.Name = "Form1";
+            this.Text = "Autodownloader";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnStart;
+        private System.Windows.Forms.Button BtnStop;
+    }
+}
+
---
+++ Autodownloader/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Autodownloader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            if (!Directory.Exists(@"C:\App"))
+            {
+                Directory.CreateDirectory(@"C:\App");
+            }
+        }
+   
+        Uri uri = new Uri("https://www.istripper.com/fileaccess/software");
+        string filename = @"C:\App\setup.exe";
+      
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            //This Code will Start Downloading the file automatically without any notice.
+            //File will be saved in the predefined directory.
+            try
+            {
+                if (File.Exists(filename))
+                {
+                    File.Delete(filename);
+                }
+                else
+                {
+                    WebClient wc = new WebClient();
+                    wc.DownloadFileAsync(uri, filename);
+
+                    wc.DownloadFileCompleted += new AsyncCompletedEventHandler(wc_DownloadFileCompleted);
+
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+
+            
+
+        }
+
+        // This method will open the command prompt and try to install the exe silently.
+        // All processes will done quietly without disturbing the end user.
+
+        public static void StartInstallationOfExeSilently()
+        {
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            startInfo.FileName = "cmd.exe";
+            startInfo.Arguments = "/K C:\\App\\setup.exe /VERYSILENT";
+            process.StartInfo = startInfo;
+            process.Start();
+            process.WaitForExit();
+        }
+
+        private void wc_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            if (e.Error == null)
+            {
+                //MessageBox.Show("Download complete!, running exe", "Completed!");
+                //Process.Start(filename);
+
+                StartInstallationOfExeSilently();
+
+            }
+            else
+            {
+                //MessageBox.Show("Unable to download exe, please check your connection", "Download failed!");
+            }
+        }
+    }
+}
